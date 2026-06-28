6fc0d8d23910ce89e8ef2e8f6e9a07706978cb05
msdosmanfred <hoefer9@gmail.com>
2026-06-28 10:32:22 +0200

    updated PKGBUILD

--- build-and-move.sh
+++ build-and-move.sh
@@ -4,5 +4,5 @@ makepkg -rsc
 sudo mv *.pkg.tar.zst /var/repo/local/
 rm -rf 86box-nightly*
 makepkg --printsrcinfo >.SRCINFO
-nightly commit -am "updated PKGBUILD"
-nightly push
+git commit -am "updated PKGBUILD"
+git push
